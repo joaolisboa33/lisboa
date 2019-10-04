52dc4aea7570eae878a23e0cc521acc69adff46f
joaolisboa33 <54724889+joaolisboa33@users.noreply.github.com>
2019-10-04 14:19:19 -0300

    Add files via upload

---
+++ ALPHA/_cabecalho.php
@@ -0,0 +1,26 @@
+<?php
+
+//////// cabecalho ////////
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="main.css">
+    link
+    <title>HOME - EMPRESA</title>
+  </head>
+  <body>
+   
+
+    
---
+++ ALPHA/index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+    ////////////////// INDEX ///////////////////////
+
+    
+    include_once ('_cabecalho.php');
+
+        echo '<p class="text-center">teste texto</p>';
+    
+    include_once ('_rodape.php');
+
+
+
+?>
+
+
